86df3837410da3f30ac83523282ce2e1e9f5be13
sdbarlow <sethdbarlow@gmail.com>
2023-03-13 12:06:56 -0600

    tests pass

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,10 @@
 
 def return_evens(num_list):
     pass
+    new_list = [x for x in num_list if not x % 2]
+    return new_list
 
 def make_exclamation(sentence_list):
+    new_list = [x + "!" for x in sentence_list]
+    return new_list
     pass
